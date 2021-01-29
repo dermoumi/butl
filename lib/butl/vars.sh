9d4331ddb3bdeecbbddd4cb9ecf1c4ff7464aad1
Dermoumi S <hi@sdrm.me>
2021-01-29 09:37:48 +0100

    feat: add to array helpers

--- lib/butl/vars.sh
+++ lib/butl/vars.sh
@@ -39,7 +39,7 @@ butl.global_array() {
     fi
 }
 
-# Copies an array to another by name, compatible with bash 3.2
+# Copies an array to another by name
 # @param source the array to copy from
 # @param target the array to copy to
 butl.copy_array() {
@@ -53,15 +53,63 @@ butl.copy_array() {
     fi
 }
 
+# Copies an associative array to another by name
+# @param source the array to copy from
+# @param target the array to copy to
+butl.copy_assiciative_array() {
+    local source=$1
+    local target=$2
+
+    local declare_statement
+    declare_statement=$(declare -p "$source" 2>/dev/null)
+
+    if ! [[ "$declare_statement" =~ ^"declare -a " ]] || eval "((\${#${source}[@]} == 0))"; then
+        eval "$target=()"
+    else
+        if ((BASH_VERSINFO[0] < 4 || (BASH_VERSINFO[0] == 4 && BASH_VERSINFO[1] < 4))); then
+            # workaround bash <4.4 quoting the content of the variables in declare's output
+            local affect_target="'$target="
+
+            # also these versions concat array elements instead of replacing them
+            # so we unset existing values before setting new ones
+            eval "$(eval "echo ${declare_statement/declare -a $source=\'/$affect_target}")"
+        else
+            eval "${declare_statement/declare -a $source=/$target=}"
+        fi
+    fi
+}
+
+# Returns the index of the first item among the rest of the items
+# returns -1 if the item is not found
+butl.array_index_of() {
+    local target=$1
+    shift
+
+    local i=0
+    while (($#)); do
+        if [[ "$1" == "$target" ]]; then
+            echo $i
+            return
+        fi
+
+        i=$((i + 1))
+        shift
+    done
+
+    echo -1
+}
+
 # checks if itm is in an array
 butl.is_in_array() {
     local target=$1
     shift
 
-    for item in "$@"; do
-        if [[ "$target" == "$item" ]]; then
+    while (($#)); do
+        if [[ "$target" == "$1" ]]; then
             return
         fi
+
+        shift
     done
 
     return 1
