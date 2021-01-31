45d382b75e71673946bc6edbf055b16207076f1e
Dermoumi S <hi@sdrm.me>
2021-01-31 23:27:32 +0100

    fix: columns fallback to replicate column binary behaviour as much as possible

--- lib/butl/columns.sh
+++ lib/butl/columns.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # Formats tab separated stdin entries as columns
+# @param out_separator:  String to separate the output with (default: '  ')
+# @param in_separator:  Single character to separate the input with (defualt '<tab>')
 butl.columns() {
-    local out_separator=${1:-  }
-    local input_separator=${2:-$'\t'}
+    local out_separator=${1:-"  "}
+    local in_separator=${2:-$'\t'}
 
-    if column -o "$out_separator" -s "$input_separator" -t -L 2>/dev/null; then
+    if column -o "$out_separator" -s "$in_separator" -t -L 2>/dev/null; then
         return
     fi
 
@@ -13,19 +15,19 @@ butl.columns() {
     local column_widths=()
     local lines=()
     local cell
-    local cell_width
 
     # for each column find the longest cell
     while IFS= read -r line; do
         if [[ "$line" ]]; then
             local i=0
             while IFS= read -r cell; do
+                local cell_width
                 cell_width="${#cell}"
                 if ((cell_width > column_widths[i])); then
                     column_widths[$i]="$cell_width"
                 fi
                 i=$((i + 1))
-            done <<<"${line//$'\t'/$'\n'}"
+            done <<<"${line//$in_separator/$'\n'}"
         fi
 
         lines+=("$line")
@@ -33,22 +35,24 @@ butl.columns() {
 
     # generate a printf string such as it can print each cells at a given width
     local printf_query
-    printf_query=$(printf -- "$out_separator%%-%ss" "${column_widths[@]::$((${#column_widths[@]} - 1))}")
-    printf_query+="$out_separator%s"
+    printf_query=$(printf -- "%%-%ss$out_separator" "${column_widths[@]::$((${#column_widths[@]} - 1))}")
+    printf_query+="%s"
 
     for line in "${lines[@]-}"; do
+        local cells=()
+
         if [[ ! "$line" ]]; then
-            echo
-            continue
+            # Replicating column behaviour
+            for cell in "${column_widths[@]}"; do
+                cells+=("")
+            done
+        else
+            while IFS= read -r cell; do
+                cells+=("$cell")
+            done <<<"${line//$in_separator/$'\n'}"
         fi
 
-        cells=()
-        while IFS= read -r cell; do
-            cells+=("$cell")
-        done <<<"${line//$input_separator/$'\n'}"
-
-        local out
         # shellcheck disable=SC2059
-        printf "${printf_query#$out_separator}\n" "${cells[@]}"
+        printf "$printf_query\n" "${cells[@]}"
     done
 }
--- tests/columns.sh
+++ tests/columns.sh
@@ -29,6 +29,52 @@ test_columns_build_columns_output_correctly() {
     assert_exits_with --code 0 --stdout="$expected_output" --stderr="" butl.columns <<<"$input"
 }
 
+test_columns_preserves_empty_lines() {
+    unset column
+    unset -f column
+
+    local input=$'money\tobsession\n\n' # double \n here
+    input+=$'unhealthy\tneglect\tof\tlife\n'
+    input+=$'better\tto\tlive\tnow'
+
+    local expected_output=$'money      obsession        \n'
+    expected_output+=$'                            \n'
+    expected_output+=$'unhealthy  neglect    of    life\n'
+    expected_output+=$'better     to         live  now'
+
+    assert_exits_with --code 0 --stdout="$expected_output" --stderr="" butl.columns <<<"$input"
+}
+
+test_columns_can_choose_input_separator() {
+    unset column
+    unset -f column
+
+    local input=$'money;obsession\n'
+    input+=$'unhealthy;neglect;of;life\n'
+    input+=$'better;to;live;now'
+
+    local expected_output=$'money      obsession        \n'
+    expected_output+=$'unhealthy  neglect    of    life\n'
+    expected_output+=$'better     to         live  now'
+
+    assert_exits_with --code 0 --stdout="$expected_output" --stderr="" butl.columns '  ' ";" <<<"$input"
+}
+
+test_columns_can_choose_output_separator() {
+    unset column
+    unset -f column
+
+    local input=$'money\tobsession\n'
+    input+=$'unhealthy\tneglect\tof\tlife\n'
+    input+=$'better\tto\tlive\tnow'
+
+    local expected_output=$'money    -obsession-    -\n'
+    expected_output+=$'unhealthy-neglect  -of  -life\n'
+    expected_output+=$'better   -to       -live-now'
+
+    assert_exits_with --code 0 --stdout="$expected_output" --stderr="" butl.columns '-' <<<"$input"
+}
+
 test_columns_falls_back_to_bash_implementation_if_columns_fails() {
     column() {
         return 127
@@ -46,7 +92,7 @@ test_columns_falls_back_to_bash_implementation_if_columns_fails() {
     assert_exits_with --code 0 --stdout="$expected_output" --stderr="" butl.columns <<<"$input"
 }
 
-test_columns_preserves_empty_lines() {
+test_columns_fallback_preserves_empty_lines() {
     column() {
         return 127
     }
@@ -56,9 +102,44 @@ test_columns_preserves_empty_lines() {
     input+=$'unhealthy\tneglect\tof\tlife\n'
     input+=$'better\tto\tlive\tnow'
 
-    local expected_output=$'money      obsession        \n\n' # double \n here
+    local expected_output=$'money      obsession        \n'
+    expected_output+=$'                            \n'
     expected_output+=$'unhealthy  neglect    of    life\n'
     expected_output+=$'better     to         live  now'
 
     assert_exits_with --code 0 --stdout="$expected_output" --stderr="" butl.columns <<<"$input"
+}
+
+test_columns_fallback_can_choose_input_separator() {
+    column() {
+        return 127
+    }
+    assert_exits_with --code 127 column
+
+    local input=$'money;obsession\n'
+    input+=$'unhealthy;neglect;of;life\n'
+    input+=$'better;to;live;now'
+
+    local expected_output=$'money      obsession        \n'
+    expected_output+=$'unhealthy  neglect    of    life\n'
+    expected_output+=$'better     to         live  now'
+
+    assert_exits_with --code 0 --stdout="$expected_output" --stderr="" butl.columns '  ' ";" <<<"$input"
+}
+
+test_columns_fallback_can_choose_output_separator() {
+    column() {
+        return 127
+    }
+    assert_exits_with --code 127 column
+
+    local input=$'money\tobsession\n'
+    input+=$'unhealthy\tneglect\tof\tlife\n'
+    input+=$'better\tto\tlive\tnow'
+
+    local expected_output=$'money    -obsession-    -\n'
+    expected_output+=$'unhealthy-neglect  -of  -life\n'
+    expected_output+=$'better   -to       -live-now'
+
+    assert_exits_with --code 0 --stdout="$expected_output" --stderr="" butl.columns '-' <<<"$input"
 }
